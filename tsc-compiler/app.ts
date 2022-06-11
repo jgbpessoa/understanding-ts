41720a666617084d038b2b0edac7bbdadaa74709
jgbpessoa <jgbpessoa@gmail.com>
2022-06-10 23:58:29 -0300

    TypeScript Compiler

---
+++ tsc-compiler/app.ts
@@ -0,0 +1,5 @@
+console.log("Lets learn about the configuration of the TypeScript Compiler");
+
+// tsc --init to create a TypeScript project
+// tsc --watch or tsc -w to compile all ts files in the project folder
+// We can add "exclude" and "include" keys in the tsconfig file to manage which files we want to compile
